21e7a2c8cd513747ec4a9bd8656e1022371e1318
Sergey <loveykosergey@ukr.net>
2023-01-14 01:44:10 +0200

    feat: add split comments and random number comments

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -2,23 +2,33 @@
   <div class="home">
     <div class="posts">
       <h2>Posts</h2>
-      <ul>
-        <li v-for="post in count_list" :key="post.id">
+      <ul >
+        <li class="posts__post" v-for="post in count_list" :key="post.id">
           <ul class="mb-3">
             <li> <h3> {{ post.title }} </h3> </li>
             <li class="pl_3">
               <ul>
-                <li  v-for="item in post.body" :key="item.id"> {{ item }} </li>
+                <li  v-for="item in post.text" :key="item.id"> {{ item }} </li>
               </ul>
             </li>
             <!-- <li>{{ item.body }}</li> -->
             <li></li>
           </ul>
+          <hr />
+          <span >Comments</span>
+
+          <ul>
+            <li class="posts__comment" v-for="item in post.posts" :key="item.id">
+              <h4>User: {{ item.name }}</h4> 
+              <ul>
+                <li v-for="item in item.posts" :key="item.id"> <i>{{ item }}</i> </li>
+              </ul>
+            </li>
+          </ul>
           <div class="post__counter">
-            <div class="post__num"> Counter letters: {{ post.body.length }} </div>
+            <div class="post__num"> Counter letters: {{ post.text.length }} </div>
             <div class="post__num"> Post № {{ post.id }} </div>
           </div>
-           <hr />
         </li>
       </ul>
     </div>
@@ -56,7 +66,7 @@ export default {
   },
 
   methods: {
-    textDevision(postsDate) {
+    postTextDevision(postsDate) {
       let arr = []
 
       for(let post of postsDate) {
@@ -66,13 +76,36 @@ export default {
         obj.userId = post.userId;
         obj.id = post.id;
         obj.title = post.title;
-        obj.body = post.body.split('\n');
+        obj.text = post.body.split('\n');
+        obj.posts = []
 
         arr.push(obj);
         
       }
 
       this.newPostDate = arr;
+    },
+
+    sortPosts(commentsData) {
+      for (let comment of commentsData) {
+        this.newPostDate[comment.postId - 1].posts.push({name: comment.name, posts: comment.body.split('\n')})
+      }
+      // console.log( 'Comments arr new:', this.newPostDate );
+    },
+
+    rendomNumberComments(data) {
+      for (let item of data) {
+        item.posts.splice(1, this.getRandomInt(2, 4));
+      }
+    },
+
+    getRandomInt(min, max) {
+      // console.log( Math.floor(Math.random() * (max - min + 1)) + min );
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    },
+
+    countLetterComments() {
+      
     },
 
     changePage(page_num) {
@@ -86,7 +119,12 @@ export default {
       }
 
       this.count_list = this.newPostDate.slice(this.pagination_offset, this.pagination_offset + 10);
-      // windows.scrollTO(0, 0);
+      
+      window.scrollTo({
+        top: 0, 
+        left: 0,
+        behavior: "smooth"
+      });
 
       // console.log( "Page number: ", this.page );
       // console.log( "Page offset: ", this.pagination_offset );
@@ -105,14 +143,26 @@ export default {
   },
 
   created() {
-    this.axios.get('/').then(response => {
+    this.axios.get('/posts').then(response => {
 
-      this.textDevision(response.data);
+      this.postTextDevision(response.data);
       // console.log( this.newPostDate );
       this.pagination_item_total = this.newPostDate.length
       // console.log( this.pagination_item_total );
 
-      this.changePage(1)
+      this.changePage(1);
+
+    });
+
+    this.axios.get('/comments').then(response => {
+
+
+      // console.log( response.data );
+      this.sortPosts(response.data);
+
+      this.rendomNumberComments(this.newPostDate);
+      // console.log( this.newPostDate );
+
 
     });
   }
